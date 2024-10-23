2abaa351a828a4ea25b910bdc6f682efc2899c1e
nimi1125 <sikaku1111@gmail.com>
2024-10-23 22:11:59 +0900

    add

---
+++ resources/views/blog/header.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+<header class="topheader">
+    <div class="hedArea">
+        <div class="hedLogoBox">
+            <h1>LOGO</h1>
+        </div>
+        <div class="d-flex">
+            <div class="hedBtnBox">
+                <a href="#">
+                    Home
+                </a>
+            </div>
+            <div class="hedBtnBox">
+                <a href="#">
+                    Create
+                </a>
+            </div>
+            <div class="hedBtnBox">
+                <a href="#">
+                    Sign in
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,15 +8,21 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.bunny.net">
+        
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
+        
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <script src="{{ asset('resources/js/app.js') }}" defer></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+            {{-- @include('layouts.navigation') --}}
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -29,7 +35,7 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                {{-- {{ $slot }} --}}
             </main>
         </div>
     </body>
