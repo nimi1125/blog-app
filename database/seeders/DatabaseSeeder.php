d1509a4c5041e7aa828e0047e5a0fc3b84fc6621
minae <minae19981013@gmail.com>
2024-10-26 10:44:16 +0900

    DatabaseSeederを追記しました

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->count(5)->create();
+
+        Category::factory()->count(10)->create();
+
+        Post::factory()->count(30)->create();
+
+        Comment::factory()->count(15)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
