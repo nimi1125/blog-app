fbe249b9aadb77c3b131a535580e8d825392d8dd
nimi1125 <sikaku1111@gmail.com>
2024-10-26 20:32:46 +0900

    update-class調整

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.guest')
 
 @section('content')
-<div class="container mx-auto mb-5">
+<div class="container mx-auto mt-5 mb-5">
     <div class="flex space-x-2">
         <input type="search" id="site-search" name="q" class="border border-gray-300 rounded-md p-2 flex-grow" placeholder="Search"/>
         <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Search</button>
