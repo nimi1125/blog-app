0af55f79b563f05d86f60d78a99f3dd13e9b2a21
minae <minae19981013@gmail.com>
2024-10-26 11:02:44 +0900

    DatabaseSeederを修正しました

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,19 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(5)->create();
-
-        Category::factory()->count(10)->create();
-
-        Post::factory()->count(30)->create();
-
-        Comment::factory()->count(15)->create();
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UsersTableSeeder::class,
+            CategoriesTableSeeder::class,
+            PostsTableSeeder::class,
+            CommentsTableSeeder::class,
+        ]);
     }
 }
