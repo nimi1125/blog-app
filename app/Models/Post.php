3a765d17c0e378a3da4b11cbe0bb7ec067b9866a
nimi1125 <sikaku1111@gmail.com>
2024-11-04 23:25:24 +0900

    update-Post.phpコメントアウト修正

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    // use HasFactory;
+    use HasFactory;
     
     protected $table = 'posts';
     protected $fillable = ['user_id','title', 'content']; 
