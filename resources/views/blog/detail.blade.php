8985673db48afbdcfda70be9dde78065fb202816
nimi1125 <sikaku1111@gmail.com>
2024-11-17 00:58:00 +0900

    update-class調整

--- resources/views/blog/detail.blade.php
+++ resources/views/blog/detail.blade.php
@@ -46,16 +46,16 @@
                 <form method="post" action="{{ route('comment.store', ['id' => $post->id]) }}" enctype="multipart/form-data" class="w-full">
                     @csrf
                     <div class="flex-grow">
+                        @if (session('success'))
+                            <div class="alert alert-success p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400">
+                                {{ session('success') }}
+                            </div>
+                        @endif
                         <textarea name="content" class="w-full h-20 p-3 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Add a comment..."></textarea>
                     </div>
                     <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
                         Comment
                     </button>
-                    @if (session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
                 </form>
             </div>
 
