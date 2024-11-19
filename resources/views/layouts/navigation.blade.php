77a08065cfad119dca2cac2a816e209ee0373fe7
minae <minae19981013@gmail.com>
2024-11-20 02:46:04 +0900

    ヘッダーのレスポンシブ調整を行いました

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,10 +11,19 @@
 
             <!-- Navigation Links -->
             <div class="shrink-0 flex items-center space-x-4 ml-auto">
-                <a href="{{ route('home') }}" class="text-white bg-black px-8 py-2 rounded-full hover:bg-gray-500">Home</a>
+                <a href="{{ route('home') }}" class="text-white bg-black px-8 py-2 rounded-full hover:bg-gray-500 hidden sm:inline-block">Home</a>
                 @if(Auth::check())
-                    <a href="{{ route('mypage') }}" class="text-white bg-black px-8 py-2 rounded-full hover:bg-gray-500">MyPage</a>
-                    <a href="{{ route('write') }}" class="text-white bg-black px-8 py-2 rounded-full hover:bg-gray-500">Create</a>
+                    <a href="{{ route('mypage') }}" class="text-white bg-black px-8 py-2 rounded-full hover:bg-gray-500 hidden sm:inline-block">MyPage</a>
+                    <a href="{{ route('write') }}" class="text-white bg-black px-6 py-2 rounded-full hover:bg-gray-500 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 mr-2">
+                            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                        </svg>Create
+                    </a>
+                @else
+                    <a href="{{ route('login') }}" class="bg-white border px-8 py-2 rounded-full hover:bg-gray-100 mr-4">{{ __('Log In') }}</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="bg-white border px-8 py-2 rounded-full hover:bg-gray-100 space-x-4">{{ __('Register') }}</a>
+                    @endif
                 @endif
             </div>
 
@@ -23,11 +32,12 @@
                 @if(Auth::check())
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
+                            {{-- ログインユーザー名のボタン --}}
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                                 <div>
                                     <p>{{ Auth::user()->name }}</p>
                                 </div>
-
+                                {{-- ログインユーザー名右横のマーク --}}
                                 <div class="ms-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -35,7 +45,7 @@
                                 </div>
                             </button>
                         </x-slot>
-
+                        {{-- プルダウン内のメニュー --}}
                         <x-slot name="content">
                             <x-dropdown-link :href="route('profile.edit')">
                                 {{ __('Profile') }}
@@ -53,57 +63,58 @@
                             </form>
                         </x-slot>
                     </x-dropdown>
+                @endif
+            </div>
 
-                    <!-- Hamburger -->
-                    <div class="-me-2 flex items-center sm:hidden">
-                        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-
-                    <!-- Responsive Navigation Menu -->
-                    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-                        <div class="pt-2 pb-3 space-y-1">
-                            <x-responsive-nav-link :href="route('mypage')" :active="request()->routeIs('mypage')">
-                                {{ __('mypage') }}
-                            </x-responsive-nav-link>
-                        </div>
-
-                        <!-- Responsive Settings Options -->
-                        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-                            <div class="px-4">
-                                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                            </div>
-
-                            <div class="mt-3 space-y-1">
-                                <x-responsive-nav-link :href="route('profile.edit')">
-                                    {{ __('Profile') }}
-                                </x-responsive-nav-link>
-
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-
-                                    <x-responsive-nav-link :href="route('logout')"
-                                            onclick="event.preventDefault();
-                                                        this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-responsive-nav-link>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                @else
-                <a href="{{ route('login') }}" class="bg-white border px-8 py-2 rounded-full hover:bg-gray-100 mr-4">{{ __('Log In') }}</a>
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="bg-white border px-8 py-2 rounded-full hover:bg-gray-100 space-x-4">{{ __('Register') }}</a>
-                    @endif
+            <!-- Hamburger -->
+            <div class="-me-2 flex items-center sm:hidden">
+                @if(Auth::check())
+                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            {{-- ハンバーガーアイコン --}}
+                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            {{-- 閉じるアイコン --}}
+                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
                 @endif
             </div>
         </div>
     </div>
+
+    <!-- Responsive Navigation Menu -->
+    @if(Auth::check())
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('mypage')" :active="request()->routeIs('mypage')">
+                    {{ __('mypage') }}
+                </x-responsive-nav-link>
+            </div>
+
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('profile.edit')">
+                        {{ __('Profile') }}
+                    </x-responsive-nav-link>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endif
 </nav>
